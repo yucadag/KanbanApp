0e0056015cb9496a5ab92c19eb9cfec9a347e40d
Mustafa Yucadağ <yucadag@gmail.com>
2021-01-01 15:21:47 +0300

    Azure Pipeline error fıxed

---
+++ KanbanApp.Api.Test/Common/DatabaseContextMock.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KanbanApp.Api.Test.Common
+{
+    public static class DatabaseContextMock<T> where T : DbContext
+    {
+        public const string SQLITE_CONNECTION_STRING = "DataSource:memory:";
+
+        public static DbContextOptions<T> InMemoryDatabase()
+        {
+            DbContextOptions<T> options = new DbContextOptionsBuilder<T>()
+                  .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                  .Options;
+
+            return options;
+        }
+
+        // Remember to open and close connection when using Sqlite
+        public static DbContextOptions<T> SQLite(SqliteConnection connection)
+        {
+            DbContextOptions<T> options = new DbContextOptionsBuilder<T>()
+                  .UseSqlite(connection)
+                  .Options;
+
+            return options;
+        }
+    }
+}
---
+++ KanbanApp.Api.Test/Common/TestBase.cs
@@ -0,0 +1,60 @@
+﻿using AutoFixture;
+using KanbanApp.Data.Concrete.EntityFramework;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System;
+using FluentValidation;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+
+namespace KanbanApp.Api.Test.Common
+{
+    //public class TestBase : IDisposable
+    //{
+    //    protected readonly IMediator _mediator;
+    //    protected readonly KanbanContext _db;
+    //    protected readonly Mock<IBackgroundJobClient> _jobClientMock;
+    //    protected readonly Fixture _fixture;
+
+    //    public TestBase()
+    //    {
+    //        var services = new ServiceCollection();
+
+    //        // Services
+    //        services.AddMediatR(typeof(Startup));
+    //      //  services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+    //      //  services.AddMvc().AddFluentValidation(cfg => { cfg.RegisterValidatorsFromAssemblyContaining<Startup>(); });
+    //        services.AddAutoMapper(typeof(Startup));
+
+
+    //        // Database
+    //        var databaseName = Guid.NewGuid().ToString();
+    //        _db = new KanbanContext(DatabaseContextMock<KanbanContext>.InMemoryDatabase());
+
+
+    //        // Global objects
+    //        _jobClientMock = new Mock<IBackgroundJobClient>();
+    //        _jobClientMock.Setup(x => x.Create(It.IsAny<Job>(), It.IsAny<EnqueuedState>()));
+
+    //        _fixture = new Fixture();
+
+
+    //        IContainer container = new Container(cfg =>
+    //        {
+    //            cfg.For<IBackgroundJobClient>().Use(_jobClientMock.Object);
+    //            cfg.For<DatabaseContext>().Use(_db);
+    //            cfg.For(typeof(ILogger<>)).Use(typeof(NullLogger<>));
+    //            cfg.Populate(services);
+    //        });
+
+    //        _mediator = container.GetInstance<IMediator>();
+    //    }
+
+    //    public void Dispose()
+    //    {
+    //        _db.Dispose();
+    //    }
+    //}
+}
